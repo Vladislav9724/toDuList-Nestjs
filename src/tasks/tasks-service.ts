0540c3739ec9583cb3420ae9f53aca10ef433b62
Vladislav9724 <vlad.tsyapka@gmail.com>
2022-11-10 20:00:27 +0200

    Remove, update task

--- src/tasks/tasks-service.ts
+++ src/tasks/tasks-service.ts
@@ -3,6 +3,7 @@ import {InjectModel} from "@nestjs/mongoose";
 import { Model} from 'mongoose'
 import {CreateTasksDto} from "./dto/create-tasks.dto";
 import {Task, TaskDocument} from "./schemas/task-schemas";
+import { UpdateTasksDto } from "./dto/update-tasks.dto";
 
 
 @Injectable()
@@ -19,6 +20,15 @@ export class TasksService{
     }
 
     async create(tasksDto: CreateTasksDto): Promise<Task>{
-        const new
+        const newTask = new this.taskModel(tasksDto)
+        return newTask.save()
+    }
+
+    async remove(id: string): Promise<Task>{
+        return this.taskModel.findByIdAndRemove(id)
+    }
+
+    async update(id: string, taskDto: UpdateTasksDto): Promise<Task>{
+        return this.taskModel.findByIdAndRemove(id, taskDto)
     }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, Delete, Get, Param, Post, Put, HttpCode, HttpStatus, H
 import {CreateTasksDto} from "./dto/create-tasks.dto";
 import {UpdateTasksDto} from "./dto/update-tasks.dto";
 import {TasksService} from "./tasks-service";
+import { Task } from "./schemas/task-schemas";
 
 @Controller('tasks')
 export class TasksController {
@@ -10,30 +11,30 @@ export class TasksController {
     }
 
     @Get()
-    getAll(){
+    getAll(): Promise<Task[]>{
         return this.tasksService.getAll()
     }
 
     @Get(':id')
-    getOne(@Param('id') id: string):string{
+    getOne(@Param('id') id: string):Promise<Task>{
        return this.tasksService.getById(id)
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
     @Header('Cache-Control', 'none')
-    create(@Body() createTasksDto: CreateTasksDto){
+    create(@Body() createTasksDto: CreateTasksDto):Promise<Task>{
         return this.tasksService.create(createTasksDto)
     }
 
     @Delete(':id')
-    remove(@Param('id') id:string){
-        return 'Remove ' + id
+    remove(@Param('id') id:string):Promise<Task>{
+        return this.tasksService.remove(id)
     }
 
     @Put(':id')
-    update(@Body() updateTaskDto: UpdateTasksDto, @Param('id')id:string){
-        return 'Updete ' + id
+    update(@Body() updateTaskDto: UpdateTasksDto, @Param('id')id:string):Promise<Task>{
+        return this.tasksService.update(id, updateTaskDto)
     }
 
 }
